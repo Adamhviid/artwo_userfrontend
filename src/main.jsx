1fdd31babdea5f6a2ece955aeefbf5e198c279c0
Rasmus <groenbek@live.dk>
2023-11-07 11:16:41 +0100

    Welp, it works

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 //import App from "./App.jsx";
 import "./index.css";
+
+// DONT FIX THIS ERRROR... SHIT WORKS...
 import Login from "./pages/login/Login";
+// DONT FIX THIS ERRROR... SHIT WORKS...
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
