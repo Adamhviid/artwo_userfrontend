b1a47040e8679540d32b7939f6ab30d6bdf55556
Rasmus <groenbek@live.dk>
2023-11-07 12:36:37 +0100

    The login landing page, however, it should probably be the register page :o

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,8 +1,52 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import "./login.css";
 import lognobackground from "../../images/logonobackground.png";
 
 const Login = () => {
+  const [firstname, setFirstname] = useState("");
+  const [lastname, setLastname] = useState("");
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleLogin = async (event) => {
+    try {
+      // WE NEED THE ENDPOINT IN ORDER FOR THIS TO WORK :)))
+      //   const response = await axios.login("http://localhost:8080/login", {
+      //     firstname: firstname,
+      //     lastname: lastname,
+      //     username: username,
+      //     email: email,
+      //     password: password,
+      //   });
+
+      //   console.log(response);
+
+      // Delete this block of code when the endpoint is ready :)))
+
+      const data = {
+        firstname: firstname,
+        lastname: lastname,
+        username: username,
+        email: email,
+        password: password,
+      };
+
+      console.log(data);
+
+      // ^^ This is where we have to delete shit
+
+      setFirstname("");
+      setLastname("");
+      setUsername("");
+      setEmail("");
+      setPassword("");
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <>
       <img className="logo" src={lognobackground} alt="Logo" />
@@ -17,6 +61,8 @@ const Login = () => {
                 id="firstname"
                 name="firstname"
                 placeholder="Fornavn"
+                value={firstname}
+                onChange={(event) => setFirstname(event.target.value)}
               />
               <input
                 className="text"
@@ -24,6 +70,8 @@ const Login = () => {
                 id="lastname"
                 name="lastname"
                 placeholder="Efternavn"
+                value={lastname}
+                onChange={(event) => setLastname(event.target.value)}
               />
               <input
                 className="text"
@@ -31,6 +79,8 @@ const Login = () => {
                 id="username"
                 name="username"
                 placeholder="Brugernavn"
+                value={username}
+                onChange={(event) => setUsername(event.target.value)}
               />
               <input
                 className="text"
@@ -38,6 +88,8 @@ const Login = () => {
                 id="email"
                 name="email"
                 placeholder="E-mail"
+                value={email}
+                onChange={(event) => setEmail(event.target.value)}
               />
               <input
                 className="text"
@@ -45,9 +97,19 @@ const Login = () => {
                 id="password"
                 name="password"
                 placeholder="Password"
+                value={password}
+                onChange={(event) => setPassword(event.target.value)}
               />
             </ul>
-            <input className="button" type="submit" value="Login" />
+            <input
+              className="button"
+              type="submit"
+              value="Login"
+              onClick={(event) => {
+                event.preventDefault();
+                handleLogin();
+              }}
+            />
           </form>
         </div>
       </div>
