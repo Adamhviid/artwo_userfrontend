0d104d2841f71c3592395bcc00b46cdb6058d54f
Rasmus <groenbek@live.dk>
2023-11-07 11:40:13 +0100

    wow

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,53 +1,57 @@
 import React from "react";
 import "./login.css";
+import lognobackground from "../../images/logonobackground.png";
 
 const Login = () => {
   return (
-    <div className="loginmain">
-      <h1>Login</h1>
-      <div className="loginform">
-        <form>
-          <ul className="logincredentials ">
-            <input
-              className="text"
-              type="text"
-              id="firstname"
-              name="firstname"
-              placeholder="Fornavn"
-            />
-            <input
-              className="text"
-              type="text"
-              id="lastname"
-              name="lastname"
-              placeholder="Efternavn"
-            />
-            <input
-              className="text"
-              type="text"
-              id="username"
-              name="username"
-              placeholder="Brugernavn"
-            />
-            <input
-              className="text"
-              type="email"
-              id="email"
-              name="email"
-              placeholder="E-mail"
-            />
-            <input
-              className="text"
-              type="password"
-              id="password"
-              name="password"
-              placeholder="Password"
-            />
-          </ul>
-          <input className="button" type="submit" value="Login" />
-        </form>
+    <>
+      <img className="logo" src={lognobackground} alt="Logo" />
+      <div className="loginmain">
+        <h1>Login</h1>
+        <div className="loginform">
+          <form>
+            <ul className="logincredentials ">
+              <input
+                className="text"
+                type="text"
+                id="firstname"
+                name="firstname"
+                placeholder="Fornavn"
+              />
+              <input
+                className="text"
+                type="text"
+                id="lastname"
+                name="lastname"
+                placeholder="Efternavn"
+              />
+              <input
+                className="text"
+                type="text"
+                id="username"
+                name="username"
+                placeholder="Brugernavn"
+              />
+              <input
+                className="text"
+                type="email"
+                id="email"
+                name="email"
+                placeholder="E-mail"
+              />
+              <input
+                className="text"
+                type="password"
+                id="password"
+                name="password"
+                placeholder="Password"
+              />
+            </ul>
+            <input className="button" type="submit" value="Login" />
+          </form>
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
